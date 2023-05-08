ce2b91348885b9c8b0f1fd37b7393878d81390ef
Diego Fernando Rodríguez Martínez <87663951+SanDieg0@users.noreply.github.com>
2023-05-08 10:27:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const Operations = {};
 
+Operations.add = function add(a,b){
+  return a+b;
+}
+
+Operations.substract = function substract(a,b){
+  return a-b;
+}
+
+Operations.multiply = function multiply(a,b){
+  return a*b;
+}
+
+Operations.divide = function divide(a,b){
+  return a/b;
+}
+
+function operate(number1, number2, operation){
+  let numer1 = prompt("Escribe el primer número");
+  let numer2 = prompt("Escribe el segundo número");
+  
+  operation(numer1, number2);
+}
